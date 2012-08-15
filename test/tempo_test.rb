22d95d1790311949b3ab1fbb3d420f95bafb1a4d
Allan Espinosa <aespinosa@cs.uhicago.edu>
2012-08-14 20:46:24 -0700

    Change test/unit to activesupport/test

--- test/tempo_test.rb
+++ test/tempo_test.rb
@@ -1,67 +1,68 @@
-require 'test/unit'
 require 'tempodb'
 require 'gsl'
 require 'time'
 require 'active_support/all'
 
-class TempoTest < Test::Unit::TestCase
-  def setup
-    @client = TempoDB::Client.new ENV['TEMPO_API_KEY'],
-                                  ENV['TEMPO_API_SECRET']
+module TempoContext; end
+def generate_datapoints
+  rng = GSL::Rng.alloc GSL::Rng::TAUS, Time.now.to_i
+
+  points = []
+  time = Time.now
+  1000.times do
+    time = 1.minute.since time
+    points << TempoDB::DataPoint.new(time, rng.gaussian)
   end
+  points
+end
 
-  def test_tempo_environment_should_be_defined
-    assert_includes ENV.keys, 'TEMPO_API_KEY'
-  end
-
-  def test_client_not_nil
-    refute_nil @client
-  end
-
-  def test_create_series
-    keys = %w(swift)
-    series = @client.get_series :keys => keys
-
-    assert_equal keys.first, series.first.key
-  end
-
-  def generate_datapoints
-    rng = GSL::Rng.alloc GSL::Rng::TAUS, Time.now.to_i
-
-    points = []
-    time = Time.now
-    1000.times do
-      time = 1.minute.since time
-      points << TempoDB::DataPoint.new(time, rng.gaussian)
-    end
-    points
-  end
-  private :generate_datapoints
-
-  def test_datapoint_generated_are_unique
+class TempoContext::DataPointTest < ActiveSupport::TestCase
+  test "datapoint generated are unique" do
     points = generate_datapoints
 
     assert_equal points.map { |point| point.ts }, 
                  points.map { |point| point.ts }.uniq
   end
 
-  def test_dump_data_to_series
+  test "data point value" do
+    point = TempoDB::DataPoint.new(Time.now, 3)
+    assert_equal 3, point.value
+  end
+
+  test "datapoint time" do
+    time = Time.now
+    point = TempoDB::DataPoint.new(time, 3)
+
+    assert_equal time, point.ts
+  end
+end
+
+class TempoContext::ClientTest < ActiveSupport::TestCase
+  def setup
+    @client = TempoDB::Client.new ENV['TEMPO_API_KEY'],
+                                  ENV['TEMPO_API_SECRET']
+  end
+
+  test "tempo environment should be defined" do
+    assert_includes ENV.keys, 'TEMPO_API_KEY'
+  end
+
+  test "client should not nil" do
+    refute_nil @client
+  end
+
+  test "create series" do
+    keys = %w(swift)
+    series = @client.get_series :keys => keys
+
+    assert_equal keys.first, series.first.key
+  end
+
+  test "dump data to series" do
     points = generate_datapoints
 
     assert_nothing_raised do
       @client.write_key "swift", points
     end
-  end
-
-  def test_datapoint_value
-    point = TempoDB::DataPoint.new(Time.now, 3)
-    assert_equal 3, point.value
-  end
-
-  def test_test_datapoint_time
-    time = Time.now
-    point = TempoDB::DataPoint.new(time, 3)
-
-    assert_equal time, point.ts
   end
 end
